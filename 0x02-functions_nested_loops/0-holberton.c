faea9dc3996548aaf0d522e80ec70197157e3ccc
Natnael <nathnaielaf01@gmail.com>
2021-06-22 11:17:15 -0700

    initial

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,20 @@
-#include<stdio.h>
+#include "holberton.h"
+
 /**
 * main - Print the word "Holberton"
 *
 * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
-printf("Holberton\n");
-return (0);
+int i;
+char p[] = "Holberton";
+
+for (i = 0; i < 9; i++)
+{
+_putchar(p[i]);
 };
+_putchar('\n');
+return (0);
+}
